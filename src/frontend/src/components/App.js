975af7ef67c497fb464db16303925c1c7fa05e62
Andy Vivallo <andresvivallo4@gmail.com>
2021-01-28 19:20:13 -0600

    Added MaterialUI

--- src/frontend/src/components/App.js
+++ src/frontend/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import HomePage from "./HomePage";
+import HomePage from "HomePage";
+import {Create} from "@material-ui/icons";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component {
   constructor(props) {
